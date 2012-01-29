2fb798dd1aa58b8cdbda02a438a5d874b113eea9
Anthony Panozzo <panozzaj@gmail.com>
2012-01-28 19:06:04 -0500

    Add some debugging information to RN-42 interface spike.

--- rn42listpairs.py
+++ rn42listpairs.py
@@ -1,37 +1,55 @@
 #!/usr/bin/python
 import sys
+import time
 import serial
 from serial.tools import list_ports
 
+BLUESMIRF_INTERFACE_STRING = 'RN42'
 
 # consider using list_ports.grep() instead
-def find_usb_port():
+def find_serial_interface():
     ports = list_ports.comports()
     for port in ports:
-        if port[0].count('usbserial') == 1:
+        if port[0].count(BLUESMIRF_INTERFACE_STRING) == 1:
+            print 'Found serial interface: %s' % port[0]
             return port[0]
     return None
 
+def connect_to_serial(serial_interface):
+    connection = serial.Serial(serial_interface, 115200, timeout=1000)
+    print 'Connected.'
+    return connection
+
+def send_command(serial_conn, command, include_newline=True):
+    print 'Sending %s' % command
+    if include_newline:
+        command += "\n"
+    serial_conn.write(command)
+
+    print 'Response:'
+    sys.stdout.write(serial_conn.readline())
+    time.sleep(0.05)
+    while serial_conn.inWaiting() > 0:
+        sys.stdout.write(serial_conn.readline())
+        time.sleep(0.05)
+    print ''
+
 def main(*args):
-    usb_port = find_usb_port()
-    if not usb_port:
+    serial_interface = find_serial_interface()
+    if not serial_interface:
         print 'USB port not found, exiting'
         return 1
 
-    print 'Found USB port: %s' % usb_port
-    print 'Connecting to it with serial...'
+    print 'Connecting to it...'
 
-    ser = serial.Serial(usb_port, 115200, timeout = 2)
+    serial_conn = connect_to_serial(serial_interface)
 
-    print 'Connected.'
+    send_command(serial_conn, "$$$", include_newline=False)
+    send_command(serial_conn, "D")
+    send_command(serial_conn, "E")
 
-    ser.write("$$$")
-    print ser.readline()
-    ser.write("D")
-    print ser.readline()
-    ser.close()
-
-    return 0 # exit errorlessly
+    serial_conn.close()
+    return 0
 
 if __name__ == '__main__':
     sys.exit(main(*sys.argv))
